9a1c75745018b5368849a67e467ff79fbb3d68ce
RiadMAC <admin@lib-sta04.aui.maincampus.root>
2021-12-01 23:28:19 +0100

    fix: seeders completed

--- SmartCity/src/App.java
+++ SmartCity/src/App.java
@@ -3,32 +3,40 @@ import smartBuilding.SmartBuildingCollection;
 import smartBuilding.FloorCollection;
 import smartBuilding.ApartmentCollection;
 
+import java.util.*;
+
 public class App {
+
   public static void main(String[] args) {
-    // // ---- SEEDERS ---- //
+    Map<Integer, SmartStreet> smartStreets = new HashMap<Integer, SmartStreet>();
+    // ---- SEEDERS ---- //
+    int count = 0;
+    smartStreets.put(++count, new SmartStreet(count, "My Smart Street 1"));
+    smartStreets.put(++count, new SmartStreet(count, "My Smart Street 2"));
 
-    SmartStreet smartStreet = new SmartStreet(0, "My Smart Street");
+    SmartStreet smartStreet = smartStreets.get(1);
 
-    // 1 Building
+      // 1 Building
     SmartBuildingCollection.addBuilding(smartStreet, "Business", "Smart Building Location", 10);
 
-    // 3 Floors
-    FloorCollection.addFloor(smartStreet, 0, 0);
-    FloorCollection.addFloor(smartStreet, 0, 1);
-    FloorCollection.addFloor(smartStreet, 0, 2);
+      // 3 Floors
+    FloorCollection.addFloor(smartStreet, 1, 0);
+    FloorCollection.addFloor(smartStreet, 1, 1);
+    FloorCollection.addFloor(smartStreet, 1, 2);
 
-    // 2 apartements in each floor
-    ApartmentCollection.addApartement(smartStreet, 0, 0);
-    ApartmentCollection.addApartement(smartStreet, 0, 1);
+      // 2 apartements in each floor
+    ApartmentCollection.addApartement(smartStreet, 1, 1);
+    ApartmentCollection.addApartement(smartStreet, 1, 1);
 
     ApartmentCollection.addApartement(smartStreet, 1, 2);
+    ApartmentCollection.addApartement(smartStreet, 1, 2);
+
+    ApartmentCollection.addApartement(smartStreet, 1, 3);
     ApartmentCollection.addApartement(smartStreet, 1, 3);
 
-    ApartmentCollection.addApartement(smartStreet, 2, 4);
-    ApartmentCollection.addApartement(smartStreet, 2, 5);
-
-    System.out.println(smartStreet.smartBuildings.get(0).floors.get(0).apartments.get(0));
-    // // ---- SEEDERS ---- //
+      // Testing if everything works by printing apartement.
+    System.out.println(smartStreet.smartBuildings.get(1).floors.get(1).apartments.get(1));
+    // ---- SEEDERS ---- //
   }
 }
 
--- SmartCity/src/smartBuilding/FloorCollection.java
+++ SmartCity/src/smartBuilding/FloorCollection.java
@@ -1,7 +1,6 @@
 package smartBuilding;
 import java.util.concurrent.atomic.AtomicInteger;
 import smartStreet.SmartStreet;
-import java.util.*;
 
 public class FloorCollection {
 	private static final AtomicInteger count = new AtomicInteger(0); // Auto-increment of Building ID;
